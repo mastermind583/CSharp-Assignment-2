ff72dd4d477648dc93147e0442187e3b58e429dc
Jacob <mastermind583@gmail.com>
2021-09-21 18:22:30 -0400

    initial upload

---
+++ TaskAppointmentManager/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Library.TaskAppointmentManager
+{
+    public class Task
+    {
+        private static int currentId = 1;
+
+        public Task()
+        {
+            Id = currentId++;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool IsCompleted { get; set; }
+        public override string ToString()
+        {
+            return $"ID: {Id} - DEADLINE: {Deadline} - NAME: {Name} - DESCRIPTION: {Description} - COMPLETED: {IsCompleted}";
+        }
+    }
+}
